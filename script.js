d4b230bf9d25ea0fdda3b57a1f9d0f3860e3d3a8
micton-code <antho_ul@hotmail.com>
2023-08-06 21:30:02 -0400

    se agregaron botones a la seccion de publicidad, y nuevas publicidades

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
 var images = document.querySelectorAll('.ad-image');
 var currentIndex = 0;
 
-function showNextImage() {
-    images[currentIndex].classList.remove('active');
-    currentIndex = (currentIndex + 1) % images.length;
-    images[currentIndex].classList.add('active');
+//Funcion para mostrar la imagen actual 
+function showCurrentImage() {
+    for (var i = 0; i < images.length; i++) {
+        if (i === currentIndex) {
+            images[i].classList.add('active');
+        } else {
+            images[i].classList.remove('active');
+        }
+    }
 }
 
-// iniciar el bucle
-setInterval(showNextImage, 6000);
+//Funcion para mostrar la imagen siguiente
+function showNextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showCurrentImage();
+}
+
+//Funcion para mostrar la imagen anterior
+function showPreviousImage() {
+    currentIndex = (currentIndex - 1 + images.length) %images.length;
+    showCurrentImage();
+}
+
+//Iniciar el bucle 
+var interval = setInterval(showNextImage, 6000);
+
+//Obtener referencias a los botones 
+var prevButton = document.getElementById('prevButton');
+var nextButton = document.getElementById('nextButton');
+
+//Agregar manejadores de eventos para los botones
+prevButton.addEventListener('click', function() {
+    clearInterval(interval); //Detener el bucle automático 
+    showPreviousImage();
+    interval = setInterval(showNextImage, 6000); //Reinicia el buble
+});
+
+nextButton.addEventListener('click', function() {
+    clearInterval(interval); //Detener el bucle automático
+    showNextImage();
+    interval = setInterval(showNextImage, 6000); //Reinicia el buble
+});
+
